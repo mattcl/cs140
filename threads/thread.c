bc41d732c52c7ed39af049f1511da4126a842ca4
samo <samo@samo-laptop.(none)>
2011-01-14 21:59:42 -0800

    Debugging Locks

--- threads/thread.c
+++ threads/thread.c
@@ -221,9 +221,7 @@ tid_t thread_create (const char *name, int priority,
 
 	/* Add to run queue. */
 	thread_unblock (t);
-	if (priority > max(thread_current()->tmp_priority, thread_current()->priority)){
-		thread_yield();
-	}
+
 	return tid;
 }
 
@@ -258,6 +256,9 @@ void thread_unblock (struct thread *t){
 	ASSERT (t->status == THREAD_BLOCKED);
 	list_push_back (&ready_list, &t->elem);
 	t->status = THREAD_READY;
+	if (t->tmp_priority > thread_current()->tmp_priority) {
+		thread_yeild();
+	}
 	intr_set_level (old_level);
 }
 
