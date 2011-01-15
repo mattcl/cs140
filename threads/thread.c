8de66189389d9a79cf1d95720a28416e534fb9da
samo <samo@samo-laptop.(none)>
2011-01-14 23:11:30 -0800

    Debugging Locks

--- threads/thread.c
+++ threads/thread.c
@@ -462,6 +462,7 @@ static void init_thread (struct thread *t, const char *name, int priority){
 	strlcpy (t->name, name, sizeof t->name);
 	t->stack = (uint8_t *) t + PGSIZE;
 	t->priority = priority;
+	t->tmp_priority = priority;
 	t->magic = THREAD_MAGIC;
 
 	//====== Begin changes=========//
