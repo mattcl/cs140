552153f29e59425a4063918a96ad16b8cdfd510a
samo <samo@samo-laptop.(none)>
2011-01-15 20:24:13 -0800

    Test 1

--- threads/thread.c
+++ threads/thread.c
@@ -748,7 +748,7 @@ static void mlfqs_insert(struct thread *t, bool reset) {
  * removes the thread from the mlfqs_queue
  */
 static void mlfqs_remove(struct thread *t) {
-	list_remove(&t->elem);
+	list_remove(&t->mlfqs_elem);
 }
 
 /**
