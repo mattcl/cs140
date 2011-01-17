a25679afebce23fdf3425dcbf8adddc5537eb361
samo <samo@samo-laptop.(none)>
2011-01-16 20:17:25 -0800

    Yay

--- threads/thread.c
+++ threads/thread.c
@@ -455,7 +455,7 @@ int thread_get_load_avg (void){
 int thread_get_recent_cpu (void){
 	/* Not yet implemented. */
 	printf("recent %d and 100 %d\n", running_thread()->recent_cpu, itof(100));
-	fixed_point fpCPU = fp_mult(itof(100), running_thread()->recent_cpu);
+	fixed_point fpCPU = fp_mult(running_thread()->recent_cpu,itof(100));
 	printf("fixed point recent cpu times 100 %d\n", fpCPU);
 	printf("FtoI of that %d\n", ftoi(fpCPU));
 	//return ftoi(fp_mult(itof(100), running_thread()->recent_cpu));
