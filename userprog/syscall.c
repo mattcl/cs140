fe88fcc50310d7c893c398ef3e1807698e40a292
samo <samo@samo-laptop.(none)>
2011-01-30 02:39:14 -0800

    Moving onto user memory

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -25,49 +25,68 @@ static void syscall_handler (struct intr_frame *f){
 
 	switch (sys_call_num){
 		case SYS_HALT:
+			printf("SYS_HALT called\n");
 			break;
 		case SYS_EXIT:
+			printf("SYS_EXIT called\n");
 			thread_exit ();
 			break;
 		case SYS_EXEC:
+			printf("SYS_EXEC called\n");
 			break;
 		case SYS_WAIT:
+			printf("SYS_WAIT called\n");
 			break;
 		case SYS_CREATE:
+			printf("SYS_CREATE called\n");
 			break;
 		case SYS_REMOVE:
+			printf("SYS_REMOVE called\n");
 			break;
 		case SYS_OPEN:
+			printf("SYS_OPEN called\n");
 			break;
 		case SYS_FILESIZE:
+			printf("SYS_FILESIZE called\n");
 			break;
 		case SYS_READ:
+			printf("SYS_READ called\n");
 			break;
 		case SYS_WRITE:
-			printf("%s",*(char**)arg(esp, 2));
+			printf("SYS_WRITE called %s",*(char**)arg(esp, 2));
 			f->eax = 4;
 			break;
 		case SYS_SEEK:
+			printf("SYS_SEEK called\n");
 			break;
 		case SYS_TELL:
+			printf("SYS_TELL called\n");
 			break;
 		case SYS_CLOSE:
+			printf("SYS_CLOSE called\n");
 			break;
 			// Project 3 Syscalls
 		case SYS_MMAP:
+			printf("SYS_MMAP called\n");
 			break;
 		case SYS_MUNMAP:
+			printf("SYS_MUNMAP called\n");
 			break;
 			//Progect 4 Syscalls
 		case SYS_CHDIR:
+			printf("SYS_CHDIR called\n");
 			break;
 		case SYS_MKDIR:
+			printf("SYS_MKDIR called\n");
 			break;
 		case SYS_READDIR:
+			printf("SYS_READDIR called\n");
 			break;
 		case SYS_ISDIR:
+			printf("SYS_ISDIR called\n");
 			break;
 		case SYS_INUMBER:
+			printf("SYS_INUMBER called\n");
 			break;
 		default:
 			PANIC ("INVALID SYS CALL NUMBER %d\n", sys_call_num);
