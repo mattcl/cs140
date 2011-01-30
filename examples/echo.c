c733627298ee81745abcabbf693ff6ab234722bb
samo <samo@samo-laptop.(none)>
2011-01-30 04:23:22 -0800

    Moving onto user memory

--- examples/echo.c
+++ examples/echo.c
@@ -5,6 +5,20 @@ int
 main (int argc, char **argv)
 {
   int i;
+
+  for (i = 0; i < argc; i++)
+    printf ("%s ", argv[i]);
+  printf ("\n");
+
+  return EXIT_SUCCESS;
+}
+e <stdio.h>
+#include <syscall.h>
+
+int
+main (int argc, char **argv)
+{
+  int i;
   printf("ECHO!!!")
   for (i = 0; i < argc; i++)
     printf ("%s ", argv[i]);
