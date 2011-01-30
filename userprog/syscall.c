273805e48e30b24a66dff816a13f224a72681a26
samo <samo@samo-laptop.(none)>
2011-01-30 14:05:44 -0800

    rolled back changes to syscall.c to see if it fixes the page fault error

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -6,7 +6,107 @@
 #include "threads/vaddr.h"
 #include "pagedir.h"
 
-/* Process identifier. */
+static void syscall_handler (struct intr_frame *);
+
+void syscall_init (void) {
+	intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+// arg with INT == 0 is the system call number
+// params are start at INT == 1
+#define arg(ESP, INT)((int *)ESP + INT)
+
+static void syscall_handler (struct intr_frame *f){
+	printf ("system call Vector number 0x%x!\n", f->vec_no);
+
+	void *esp = f->esp;
+	int sys_call_num = *(int*)arg(esp, 0);
+
+	printf("syscall esp %p\n", esp);
+	printf("System number %d\n",sys_call_num);
+
+	switch (sys_call_num){
+		case SYS_HALT:
+			printf("SYS_HALT called\n");
+			break;
+		case SYS_EXIT:
+			printf("SYS_EXIT called\n");
+			thread_exit ();
+			break;
+		case SYS_EXEC:
+			printf("SYS_EXEC called\n");
+			break;
+		case SYS_WAIT:
+			printf("SYS_WAIT called\n");
+			break;
+		case SYS_CREATE:
+			printf("SYS_CREATE called\n");
+			break;
+		case SYS_REMOVE:
+			printf("SYS_REMOVE called\n");
+			break;
+		case SYS_OPEN:
+			printf("SYS_OPEN called\n");
+			break;
+		case SYS_FILESIZE:
+			printf("SYS_FILESIZE called\n");
+			break;
+		case SYS_READ:
+			printf("SYS_READ called\n");
+			break;
+		case SYS_WRITE:{
+			struct thread *t = thread_current ();
+			printf("SYS_WRITE called %s", *(char**)arg(esp, 2));
+			f->eax = 4;
+			break;
+		}
+		case SYS_SEEK:
+			printf("SYS_SEEK called\n");
+			break;
+		case SYS_TELL:
+			printf("SYS_TELL called\n");
+			break;
+		case SYS_CLOSE:
+			printf("SYS_CLOSE called\n");
+			break;
+			// Project 3 Syscalls
+		case SYS_MMAP:
+			printf("SYS_MMAP called\n");
+			break;
+		case SYS_MUNMAP:
+			printf("SYS_MUNMAP called\n");
+			break;
+			//Progect 4 Syscalls
+		case SYS_CHDIR:
+			printf("SYS_CHDIR called\n");
+			break;
+		case SYS_MKDIR:
+			printf("SYS_MKDIR called\n");
+			break;
+		case SYS_READDIR:
+			printf("SYS_READDIR called\n");
+			break;
+		case SYS_ISDIR:
+			printf("SYS_ISDIR called\n");
+			break;
+		case SYS_INUMBER:
+			printf("SYS_INUMBER called\n");
+			break;
+		default:
+			PANIC ("INVALID SYS CALL NUMBER %d\n", sys_call_num);
+			break;
+	}
+}
+
+/*#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "pagedir.h"
+
+/* Process identifier. *
 typedef int pid_t;
 #define PID_ERROR ((pid_t) -1)
 
@@ -117,11 +217,11 @@ static void syscall_handler (struct intr_frame *f){
 	}
 }
 
-/*
+ *
  * Takes a user program pointer and checks it, will return NULL if the pointer is invalid
  * Otherwise it will return the address that the kernel can use to access the appropriate data
  * Should be called before ever dereferencing a user pointer
- */
+ *
 static void *convert_user_pointer (void *user_ptr UNUSED){
 
 }
@@ -173,3 +273,4 @@ static void system_tell(struct intr_frame *f, int fd UNUSED){
 static void system_close(struct intr_frame *f, int fd UNUSED){
 	printf("SYS_CLOSE called\n");
 }
+*/
