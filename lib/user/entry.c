be32effdbb8bfdce289462155b2db21c256aef42
samo <samo@samo-laptop.(none)>
2011-01-30 01:21:57 -0800

    Removed printfs

--- lib/user/entry.c
+++ lib/user/entry.c
@@ -4,6 +4,5 @@ int main (int, char *[]);
 void _start (int argc, char *argv[]);
 
 void _start (int argc, char *argv[]){
-	printf("Entry\n");
 	exit (main (argc, argv));
 }
--- userprog/process.c
+++ userprog/process.c
@@ -85,7 +85,9 @@ static void start_process (void *file_name_) {
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int process_wait (tid_t child_tid UNUSED){
-  return -1;
+	while(1){
+		;
+	}
 }
 
 /* Free the current process's resources. */
