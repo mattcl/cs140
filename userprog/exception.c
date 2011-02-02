b1bcdb78b5510683dcfda2f0a20d515dac7058e2
samo <samo@samo-laptop.(none)>
2011-02-02 01:48:54 -0800

    Syscall backed up and prints completely removed

--- userprog/exception.c
+++ userprog/exception.c
@@ -147,11 +147,11 @@ static void page_fault (struct intr_frame *f){
 		/* To implement virtual memory, delete the rest of the function
 	     body, and replace it with code that brings in the page to
 	     which fault_addr refers. */
-		 printf ("Page fault at %p: %s error %s page in %s context.\n",
+		 /*printf ("Page fault at %p: %s error %s page in %s context.\n",
 				fault_addr,
 				not_present ? "not present" : "rights violation",
 					  write ? "writing" : "reading",
-				       user ? "user" : "kernel");
+				       user ? "user" : "kernel");*/
 		kill(f);
 	} else {
 		f->eip = (void*)f->eax;
