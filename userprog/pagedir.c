ebd142240485257bdcfcfa17acee8f59c16af2ce
samo <samo@samo-laptop.(none)>
2011-02-19 22:53:58 -0800

    Frame.h/c

--- userprog/pagedir.c
+++ userprog/pagedir.c
@@ -5,7 +5,7 @@
 #include "threads/init.h"
 #include "threads/pte.h"
 #include "threads/palloc.h"
-
+#include "vm/frame.h"
 static uint32_t *active_pd (void);
 static void invalidate_pagedir (uint32_t *);
 
@@ -38,7 +38,7 @@ void pagedir_destroy (uint32_t *pd){
 
 			for(pte = pt; pte < pt + PGSIZE / sizeof *pte; pte++){
 				if(*pte & PTE_P){
-					palloc_free_page (pte_get_page (*pte));
+					frame_clear_page(pte_get_page (*pte));
 				}
 			}
 			palloc_free_page (pt);
