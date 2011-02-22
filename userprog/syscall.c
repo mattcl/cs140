cadc654f9752b5f1aa1c666fdbe28ba9d4c6122b
samo <samo@samo-laptop.(none)>
2011-02-22 02:04:50 -0800

    debuggin time

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -482,13 +482,16 @@ static bool buffer_is_valid (const void * buffer, unsigned int size){
 
 static bool buffer_is_valid_writable (void * buffer, unsigned int size){
 	uint8_t *uaddr = (uint8_t*)buffer;
-	if(!is_user_vaddr(uaddr) || put_user(uaddr, 1) < 0){
+	int byte;
+	if(!is_user_vaddr(uaddr) || (byte = get_user(uaddr)) < 0 || put_user(uaddr, 1) < 0){
 		return false;
 	}
+	put_user(uaddr, byte);
 	uaddr += size;
-	if(!is_user_vaddr(uaddr) || put_user(uaddr, 1) < 0){
+	if(!is_user_vaddr(uaddr) || (byte = get_user(uaddr)) < 0 || put_user(uaddr, 1) < 0){
 		return false;
 	}
+	put_user(uaddr, byte);
 	return true;
 }
 
