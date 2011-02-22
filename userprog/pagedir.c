c67533c0771b0451bf4ce3ae545ab6caaea71639
samo <samo@samo-laptop.(none)>
2011-02-21 18:54:14 -0800

    debuggin time

--- userprog/pagedir.c
+++ userprog/pagedir.c
@@ -345,7 +345,8 @@ medium_t pagedir_get_medium (uint32_t *pd, const void *uaddr){
 		}else if((*pte & (uint32_t)PTE_AVL) == PTE_AVL_MMAP){
 			return PTE_AVL_MMAP;
 		}else{
-			PANIC("pagedir_get_medium found unexpected medium %u. %p faulting address", (*pte & (uint32_t)PTE_AVL), uaddr);
+			//PANIC("pagedir_get_medium found unexpected medium %u. %p faulting address", (*pte & (uint32_t)PTE_AVL), uaddr);
+			return PTE_AVL_MEMORY;
 		}
 	}else{
 		PANIC("pagedir_get_medium called on a page table entry that is not initialized");
