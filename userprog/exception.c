1931666df834b832672a265998bcc53aab2c34c2
samo <samo@samo-laptop.(none)>
2011-02-24 00:16:02 -0800

    New commit message

--- userprog/exception.c
+++ userprog/exception.c
@@ -175,6 +175,8 @@ static void page_fault (struct intr_frame *f){
 		   we can increment it easily*/
 		uint8_t *uaddr = (uint8_t*)(((uint32_t)fault_addr & PTE_ADDR));
 
+		printf("Medium is %x dirty is %u, swap is %x\n", type, pagedir_is_dirty(thread_current()->pagedir,fault_addr ), PTE_AVL_SWAP);
+
 		if(type == PTE_AVL_SWAP){
 			/* Data is not present but on swap read it in
 							   then return so that dereference becomes valid*/
