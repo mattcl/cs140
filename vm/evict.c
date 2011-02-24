028af975de3ff93ffa9eb10bd5d54a0d74a39395
samo <samo@samo-laptop.(none)>
2011-02-23 22:39:53 -0800

    New commit message

--- vm/evict.c
+++ vm/evict.c
@@ -41,6 +41,7 @@ void *evict_page(void *uaddr){
 	struct frame_entry *frame ;
 	struct frame_entry *frame_to_clear;
 	while((evict_hand + threshold) % frame_table_size() < clear_hand -1){
+		printf("evict %u, clear %u\n", evict_hand, clear_hand);
 		/* Our clear hand is still at least theshold bits in front of us */
 		frame= frame_at_position(evict_hand);
 		evict_hand ++;
@@ -58,6 +59,7 @@ void *evict_page(void *uaddr){
 	/* in this case we need to move both hands simultaneously until the
        evict_hand finds a !accessed page */
 	while(true){
+		printf("evict %u, clear %u\n", evict_hand, clear_hand);
 		frame = frame_at_position(evict_hand);
 		frame_to_clear = frame_at_position(clear_hand);
 
