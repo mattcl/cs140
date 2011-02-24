d7ed1392eb58c6cebddb6efd12b380d1556d9bdd
John-Ashton Allen <bglp05@myth3.Stanford.EDU>
2011-02-24 00:50:36 -0800

    またかよ

--- vm/evict.c
+++ vm/evict.c
@@ -37,21 +37,12 @@ static void *relocate_page (struct frame_entry *f, void * uaddr);
    the kernel virtual address so that the user can install this
    kernel virtual address into its pagedirectory */
 void *evict_page(void *uaddr){
-<<<<<<< HEAD:vm/evict.c
-
-	printf("Evicting For uaddr %p\n", uaddr);
-	struct frame_entry *frame ;
-	struct frame_entry *frame_to_clear;
-	while((evict_hand + threshold) % frame_table_size() < clear_hand % frame_table_size()){
-		printf("1 evict %u, clear %u\n", evict_hand % frame_table_size(), clear_hand % frame_table_size());
-
-=======
 //	printf("Evicting For uaddr %p\n", uaddr);
 	struct frame_entry *frame ;
 	struct frame_entry *frame_to_clear;
 	while((evict_hand + threshold) % frame_table_size() < clear_hand % frame_table_size()){
-		//printf("1 evict %u, clear %u\n", evict_hand % frame_table_size(), clear_hand % frame_table_size());
->>>>>>> c0a7aa746d0583e3831a1cc93ba2a03a0b0ad87a:vm/evict.c
+ //printf("1 evict %u, clear %u\n", evict_hand % frame_table_size(), clear_hand % frame_table_size());
+
 		/* Our clear hand is still at least theshold bits in front of us */
 		frame= frame_at_position(evict_hand % frame_table_size());
 		evict_hand ++ ;//= (evict_hand + 1) % frame_table_size();;
@@ -98,16 +89,10 @@ void clear_until_threshold(void){
 }
 
 static void *relocate_page (struct frame_entry *f, void * uaddr){
-<<<<<<< HEAD:vm/evict.c
-
-
-	medium_t medium = pagedir_get_medium(f->cur_pagedir,f->uaddr);
-	//	printf("uaddr of frame we are evicting %x\n", f->uaddr);
-=======
 	//printf("Relocate page , with evicthand %u and clear_hand %u\n", evict_hand % frame_table_size(), clear_hand % frame_table_size());
 	medium_t medium = pagedir_get_medium(f->cur_pagedir,f->uaddr);
 	//printf("uaddr of frame we are evicting %x\n", f->uaddr);
->>>>>>> c0a7aa746d0583e3831a1cc93ba2a03a0b0ad87a:vm/evict.c
+
 	ASSERT(medium != PTE_AVL_ERROR);
 
 	void *kaddr = palloc_get_kaddr_user_index(f->position_in_bitmap);
@@ -166,11 +151,9 @@ static void *relocate_page (struct frame_entry *f, void * uaddr){
 	f->uaddr = uaddr;
 	f->cur_pagedir = thread_current()->pagedir;
 	f->cur_thread = thread_current();
-<<<<<<< HEAD:vm/evict.c
-	//	printf("Returned %p\n", kaddr);
-=======
+
 	//printf("Returned %p\n", kaddr);
->>>>>>> c0a7aa746d0583e3831a1cc93ba2a03a0b0ad87a:vm/evict.c
+
 	return kaddr;
 }
 
