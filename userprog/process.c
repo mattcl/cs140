f179498e0e8935eca3e09eff2f0dd03d8cece694
samo <samo@samo-laptop.(none)>
2011-02-26 02:22:52 -0800

    stuff

--- userprog/process.c
+++ userprog/process.c
@@ -418,7 +418,9 @@ void process_exit (void){
 
 	/* Free all of the swap slots that are currently occupied
 	   by this process */
+	printf("destroying hash table\n");
 	hash_destroy(&cur_process->swap_table, &swap_slot_destroy);
+	printf("destroyed hash table\n");
 
 	/* We do not need to lock this because all children of
  	   this process need to go through acquiring a handle
