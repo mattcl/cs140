7e344f8489df1377aaea462b40e5ff83c4579315
samo <samo@samo-laptop.(none)>
2011-02-26 10:02:35 -0800

    stuff

--- threads/thread.c
+++ threads/thread.c
@@ -645,7 +645,7 @@ void thread_schedule_tail (struct thread *prev){
 	cur->status = THREAD_RUNNING;
 #ifdef USERPROG
 	/* Activate the new address space. */
-	//process_activate ();
+	process_activate ();
 #endif
 	/* prev and cur can't be the same and dying or we will
 	 * reach Non-reachable code as a thread that is dying
@@ -688,7 +688,7 @@ static void schedule (void){
 	   returning from switch threads in the new
 	   thread */
 #ifdef USERPROG
-	pagedir_activate (next->pagedir);
+	//pagedir_activate (next->pagedir);
 #endif
 
 	if(cur != next){
