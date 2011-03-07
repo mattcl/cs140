5672848b2bb64a9275aec20abe42d22bc7c6a093
samo <samo@samo-laptop.(none)>
2011-03-06 22:07:08 -0800

    Buffer cache implemented let the debugging begin

---
+++ lib/kernel/uint_set.c
@@ -0,0 +1,56 @@
+#include "uint_set.h"
+
+static unsigned int_set_hash_func(const struct hash_elem*a, void *aux UNUSED){
+	uint32_t key = hash_entry(a, struct uint_set_entry, e)->key;
+	return hash_bytes(&key, (sizeof(uint32_t)));
+}
+
+static bool int_set_hash_comp(const struct hash_elem *a,
+							  const struct hash_elem *b, void *aux UNUSED){
+	ASSERT(a != NULL);
+	ASSERT(b != NULL);
+	return (hash_entry(a, struct uint_set_entry, e)->key <
+			hash_entry(a, struct uint_set_entry, e)->key);
+}
+
+static void int_set_hash_destroy(struct hash_elem *e, void *aux UNUSED){
+	free(hash_entry(e, struct uint_set_entry, e));
+}
+
+bool uint_set_init(struct uint_set *set){
+	return hash_init(set->set_hash, int_set_hash_func, int_set_hash_comp);
+}
+
+bool uint_set_is_member(struct uint_set *set, uint32_t key){
+	struct uint_set_entry entry;
+	entry.key = key;
+	struct hash_elem *res = hash_find(&set->set_hash, &entry.e);
+	if(res != NULL){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+void uint_set_add_member(struct uint_set *set, uint32_t key){
+	if(!uint_set_is_member(key)){
+		struct uint_set_entry *e = calloc(1, sizeof(struct uint_set_entry));
+		e->key = key;
+		hash_insert(&set->set_hash, e->e);
+	}
+}
+
+void uint_set_remove(struct uint_set *set, uint32_t key){
+	struct uint_set_entry entry;
+	entry.key = key;
+	struct hash_elem *del = hash_delete(&set->set_hash, entry->e);
+	if(del != NULL){
+		free(hash_entry(del, struct uint_set_entry, e));
+	}
+}
+
+void uint_set_destroy(struct uint_set *set){
+	hash_destroy(set, &int_set_hash_destroy);
+}
+
+
---
+++ lib/kernel/uint_set.h
@@ -0,0 +1,31 @@
+#ifndef INT_SET_H_
+#define INT_SET_H_
+
+/* I really just want a set damn it */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include "hash.h"
+#include <debug.h>
+
+struct uint_set{
+	struct hash set_hash;
+};
+
+struct uint_set_entry{
+	uint32_t key;
+	struct hash_elem e;
+};
+
+bool uint_set_init(struct uint_set *set);
+
+bool uint_set_is_member(struct uint_set *set, uint32_t key);
+
+void uint_set_add_member(struct uint_set *set, uint32_t key);
+
+void uint_set_remove(struct uint_set *set, uint32_t key);
+
+void uint_set_destroy(struct uint_set *set);
+
+
+#endif /* INT_SET_H_ */
