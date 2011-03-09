299b7b1750927c9842f37be1c443e83db9095b8b
samo <samo@samo-laptop.(none)>
2011-03-09 00:46:43 -0800

    New Branch

--- filesys/filesys.c
+++ filesys/filesys.c
@@ -89,7 +89,7 @@ struct file * filesys_open (const char *path){
 	struct dir *dir = dir_open_path (path, &file_name);
 	struct inode *inode = NULL;
 	printf("filesys open dir_open path success name is %s\n", file_name);
-	pritnf("dir == root %u\n", dir == dir_open_root());
+	printf("dir sector %u root sector %u\n", dir->sector, ROOT_DIR_SECTOR);
 	if(dir != NULL){
 		dir_lookup (dir, file_name, &inode);
 	}
