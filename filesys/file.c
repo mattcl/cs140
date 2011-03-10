d9f32e95e0f63012b2fce4201ef060b103c1b422
samo <samo@samo-laptop.(none)>
2011-03-10 15:21:08 -0800

    down to two failures in persistence

--- filesys/file.c
+++ filesys/file.c
@@ -75,6 +75,7 @@ off_t file_read_at (struct file *file, void *buffer, off_t size, off_t file_ofs)
    not yet implemented.)
    Advances FILE's position by the number of bytes read. */
 off_t file_write (struct file *file, const void *buffer, off_t size){
+	printf("file write\n");
 	off_t bytes_written = inode_write_at (file->inode, buffer, size, file->pos);
 	file->pos += bytes_written;
 	return bytes_written;
