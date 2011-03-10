72825b0eda28cc19859b2617f060623a9e203dcf
samo <samo@samo-laptop.(none)>
2011-03-10 02:52:18 -0800

    merge

--- tests/filesys/extended/dir-vine.c
+++ tests/filesys/extended/dir-vine.c
@@ -17,7 +17,7 @@ void test_main (void){
   int i;
 
   msg ("creating many levels of files and directories...");
-  quiet = false;
+  quiet = true;
   CHECK (mkdir ("start"), "mkdir \"start\"");
   CHECK (chdir ("start"), "chdir \"start\"");
   for(i = 0; ; i++){
@@ -71,7 +71,7 @@ void test_main (void){
   quiet = false;
 
   msg ("removing all but top 10 levels of files and directories...");
-  quiet = false;
+  quiet = true;
   while(i-- > 10){
       char file_name[16], dir_name[16];
       snprintf (file_name, sizeof file_name, "file%d", i);
