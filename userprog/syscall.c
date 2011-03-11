6be20828eed944bae0deba7ffa130dfb7daadb85
samo <samo@samo-laptop.(none)>
2011-03-11 01:18:34 -0800

    Im finding these bugs by 1 or else I'm going to sleep

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -922,11 +922,9 @@ static void pin_all_frames_for_buffer(const void *buffer, unsigned int size){
 		while(!pagedir_is_present(pd, uaddr) || !pin_frame_entry(kaddr = pagedir_get_page(pd, uaddr))){
 			/* Generate a page fault to get the page read
 			   in so that we can pin it's frame */
-			//printf("Infinite loop?\n");
-			intr_enable();
+			printf("Infinite loop?\n");
 			int x = get_user(uaddr);
-			intr_disable();
-			//printf("x %d %p\n", x, kaddr);
+			printf("x %d %p\n", x, kaddr);
 		}
 		intr_enable();
 	}
