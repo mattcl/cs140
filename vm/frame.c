0988eae04884666fc47e1cccfd32b5079e0fd84a
samo <samo@samo-laptop.(none)>
2011-03-11 01:42:20 -0800

    Im finding these bugs by 1 or else I'm going to sleep

--- vm/frame.c
+++ vm/frame.c
@@ -379,7 +379,8 @@ void unpin_frame_entry(void *kaddr){
 bool pin_frame_entry(void *kaddr){
 	if(kaddr < f_table.base ||
 			(uint8_t*)kaddr  >= (uint8_t*)f_table.base + (f_table.size * PGSIZE)){
-		PANIC("kaddr %p not between base %p and end %p \n", kaddr, (uint8_t*)f_table.base, (uint8_t*)f_table.base + (f_table.size * PGSIZE));
+		PANIC("kaddr %p not between base %p and end %p \n", kaddr,
+				(uint8_t*)f_table.base, (uint8_t*)f_table.base + (f_table.size * PGSIZE));
 	}
 	lock_acquire(&f_table.frame_table_lock);
 	struct frame_entry *entry = frame_entry_at_kaddr(kaddr);
