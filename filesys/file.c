309f19ea00bc7668b1f1a7254c828c77cad36a43
samo <samo@samo-laptop.(none)>
2011-03-10 17:02:06 -0800

    changed bytes to sector from a complicated mess to a clean and simple recursive function

--- filesys/file.c
+++ filesys/file.c
@@ -75,9 +75,9 @@ off_t file_read_at (struct file *file, void *buffer, off_t size, off_t file_ofs)
    not yet implemented.)
    Advances FILE's position by the number of bytes read. */
 off_t file_write (struct file *file, const void *buffer, off_t size){
-	printf("file write\n");
+	//printf("file write\n");
 	off_t bytes_written = inode_write_at (file->inode, buffer, size, file->pos);
-	printf("file write return\n");
+	//printf("file write return\n");
 	file->pos += bytes_written;
 	return bytes_written;
 }
